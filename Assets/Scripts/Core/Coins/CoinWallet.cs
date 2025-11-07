192bf7a73740a648d80f9234eb643997c87eb8a1
dkiexe <dekuffzain@gmail.com>
2025-11-07 15:03:24 +0000

    Section 2 - Lecture 20

---
+++ Assets/Scripts/Core/Coins/CoinWallet.cs
@@ -0,0 +1,17 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinWallet : NetworkBehaviour
+{
+    public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Coin>(out Coin CoinComponent))
+        {
+            int CoinValue = CoinComponent.Collect();
+            if (!IsServer) return;
+            TotalCoins.Value += CoinValue;
+        }
+    }
+}
