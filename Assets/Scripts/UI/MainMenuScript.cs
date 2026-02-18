88e80d6bbc863e7856edf549051c8124fc3d1efb
David <Davids714506@gmail.com>
2026-02-18 13:41:15 +0000

    Section 5 - Lecture 68 Tweaked all the buttons to disable when the client is performing a network action (connecting to game / creating game )

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
@@ -7,9 +8,15 @@ public class MainMenuScript : MonoBehaviour
     [SerializeField] private TMP_Text queueTimerText;
     [SerializeField] private TMP_Text findMatchButtonText;
     [SerializeField] private TMP_InputField joinCodeField;
+    [SerializeField] private Button MM_Button;
+    [SerializeField] private Button HostButton;
+    [SerializeField] private Button ClientButton;
+    [SerializeField] private Button LobbiesButton;
+    [SerializeField] private Button LanButton;
 
     private void Start()
     {
+        SetButtonsActive(true);
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto); // Reset the cursor back to default mouse.
         queueStatusText.text = string.Empty;
         queueTimerText.text = string.Empty;
@@ -17,25 +24,32 @@ public class MainMenuScript : MonoBehaviour
 
     public async void StartRelayHost()
     {
+        SetButtonsActive(false, true);
         await HostSingelton.Instance.GameManager.StartRelayHostAsync();
+        SetButtonsActive(true, true);
     }
 
     public async void StartRelayClient()
     {
+        SetButtonsActive(false);
         await ClientSingelton.Instance.GameManager.StartRelayClientAsync(joinCodeField.text);
+        SetButtonsActive(true);
     }
 
     public async void StartMatchMakerClient()
     {
+        SetButtonsActive(false);
         await ClientSingelton.Instance.GameManager.StartMatchmakerClientAsync(
             queueStatusText, 
             queueTimerText,
             findMatchButtonText
             );
+        SetButtonsActive(true);
     }
 
     public void StartLanPlay()
     {
+        SetButtonsActive(false);
         if (!LanPortCheck.IsPortUsed())
         {
             HostSingelton.Instance.GameManager.StartLanHost();
@@ -44,5 +58,14 @@ public class MainMenuScript : MonoBehaviour
         {
             ClientSingelton.Instance.GameManager.StartClient();
         }
+    }
+
+    private void SetButtonsActive(bool status, bool includeMM = false)
+    {
+        if (includeMM) MM_Button.interactable = status;
+        HostButton.interactable = status;
+        ClientButton.interactable = status;
+        LobbiesButton.interactable = status;
+        LanButton.interactable = status;
     }
 }
