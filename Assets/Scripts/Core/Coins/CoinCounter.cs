f074e36cc0bb3b6495ad0178d0d5bcbf74037b36
dkiexe <dekuffzain@gmail.com>
2026-03-23 06:58:38 +0000

    WIP added slow time effect and winner announcement, needs a script to end game.

--- Assets/Scripts/Core/Coins/CoinCounter.cs
+++ Assets/Scripts/Core/Coins/CoinCounter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -5,7 +7,7 @@ using UnityEngine;
 public class CoinCounter : NetworkBehaviour
 {
     [Header("Refrences")]
-    [SerializeField] private TextMeshPro gameGoalText;
+    [SerializeField] private TextMeshProUGUI gameGoalText;
 
     [Header("Wining Settings")]
     [SerializeField] private int CoinsToWin = 30;
@@ -23,16 +25,36 @@ public class CoinCounter : NetworkBehaviour
 
         if (coinWallet.TryGetComponent<TankPlayer>(out TankPlayer tankPlayer))
         {
-            Time.timeScale = 0f;   // Freeze
+            StartCoroutine(SlowTime());
             if (tankPlayer.PlayerColor != null)
             {
                 gameGoalText.color = tankPlayer.PlayerColor.Value; // {_(!)_} Null here when not matchmaking.
             }
             else
             {
-                gameGoalText.color = Color.green;
+                gameGoalText.color = Color.gold;
             }
             gameGoalText.text = $"Player {tankPlayer.PlayerName.Value} Wins!";
+            Endgame(tankPlayer);
+        }
+    }
+
+    private void Endgame(TankPlayer winner)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private IEnumerator SlowTime()
+    {
+        float current_ts, startScale;
+        current_ts = startScale = Time.timeScale;
+
+        while (current_ts >= 0)
+        {
+            Time.timeScale = current_ts;
+            current_ts -= 0.25f;
+            yield return new WaitForSeconds((current_ts - startScale) * -1);
         }
     }
 
